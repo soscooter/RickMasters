34fb1bc118896d8ddf74fbbcf700fc5cd68b82d8
soscooter <lelya.rumynin@gmail.com>
2025-05-26 13:44:20 +0300

    add UIColor extension and fonts

--- RickMasters/ViewController.swift
+++ RickMasters/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import PinLayout
 
 class ViewController: UIViewController {
-
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Статистика"
+        label.font = UIFont(name: "Gilroy-ExtraBold", size: 32)
+        //        label.textColor = UIColor(hex: "#2D2D2DFF")
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor(hex: "#F6F6F6FF")
+//        setupView()
     }
+    
+    private func setupView(){
+        
+        view.addSubview(titleLabel)
+        titleLabel.pin
+//            .top(to: .safeArea).marginTop(48)
+            .horizontally(16)
+            .sizeToFit()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
+        let safeTop = view.safeAreaInsets.top
 
+        view.addSubview(titleLabel)
+        titleLabel.pin
+            .top(safeTop + 48)
+            .horizontally(16)
+            .sizeToFit()
+    }
+    
 }
-
